b7920a758f747b2e0f5e17318933c7c306389612
KafiRijal <kafirs75@gmail.com>
2024-04-20 23:32:13 +0700

    add database

---
+++ praktikum6/db_puskesmas.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 20, 2024 at 06:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_puskesmas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelurahan`
+--
+
+CREATE TABLE `kelurahan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `kec_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelurahan`
+--
+
+INSERT INTO `kelurahan` (`id`, `nama`, `kec_id`) VALUES
+(1, 'Cipayung', 1),
+(2, 'Ciracas', 1),
+(3, 'Cimanggis', 2),
+(4, 'Cibubur', 2),
+(5, 'Cijantung', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(10) NOT NULL,
+  `nama` varchar(45) NOT NULL,
+  `tmp_lahir` varchar(30) DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `gender` enum('L','P') DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `kelurahan_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `kode`, `nama`, `tmp_lahir`, `tgl_lahir`, `gender`, `email`, `alamat`, `kelurahan_id`) VALUES
+(1, 'P001', 'Kafi Rijal', '', '0000-00-00', 'L', 'kafi@gmail.com', 'Jalan Lapangan Member', 1),
+(2, 'P002', 'Kapoy', NULL, NULL, 'L', 'kapoy@gmail.com', 'Jalan Kampung Sidamukti', 5),
+(3, 'P003', 'Nayanika', NULL, NULL, 'P', 'nayanika@gmail.com', 'Jalan Bogol', 5),
+(4, 'P004', 'Zoya', NULL, NULL, 'P', 'zoya@gmail.com', 'Jalan Tanjung Pinang', 3),
+(5, 'P005', 'Daffa', NULL, NULL, 'L', 'daffa@gmail.com', 'Jalan Pondok', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kode` (`kode`),
+  ADD KEY `fk_pasien_kelurahan` (`kelurahan_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kelurahan`
+--
+ALTER TABLE `kelurahan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD CONSTRAINT `fk_pasien_kelurahan` FOREIGN KEY (`kelurahan_id`) REFERENCES `kelurahan` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
